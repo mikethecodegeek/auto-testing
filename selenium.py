a1360cfa141ee2a232b295429dd647c1db42d7ed
mike <mikethecodegeek@gmail.com>
2021-04-08 10:16:58 -0700

    first commit

---
+++ selenium.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+# Initiate the browser
+browser  = webdriver.Chrome()
+
+browser.get('https://www.huntr.co/')
